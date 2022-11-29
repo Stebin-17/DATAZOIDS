8a951de32884fc9c3652c8fba8a1b7b35210cb82
SUNNY KUMAR <75533816+sunnysisodhya1998@users.noreply.github.com>
2022-11-29 11:33:56 +0530

    added quiz file

---
+++ src/quiz/application/Quiz.java
@@ -0,0 +1,313 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Quiz extends JFrame implements ActionListener {
+    
+    String questions[][] = new String[10][5];
+    String answers[][] = new String[10][2];
+    String useranswers[][] = new String[10][1];
+    JLabel qno, question;
+    JRadioButton opt1, opt2, opt3, opt4;
+    ButtonGroup groupoptions;
+    JButton next, submit, lifeline;
+    
+    public static int timer = 15;
+    public static int ans_given = 0;
+    public static int count = 0;
+    public static int score = 0;
+    
+    String name;
+    
+    Quiz(String name) {
+        this.name = name;
+        setBounds(50, 0, 1440, 850);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/quiz.jpg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1440, 392);
+        add(image);
+        
+        qno = new JLabel();
+        qno.setBounds(100, 450, 50, 30);
+        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(qno);
+        
+        question = new JLabel();
+        question.setBounds(150, 450, 900, 30);
+        question.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(question);
+        
+        questions[0][0] = "Which of the following is not a part of the data science process?";
+        questions[0][1] = "Communication Building";
+        questions[0][2] = "Operationalize";
+        questions[0][3] = "Model Planning";
+        questions[0][4] = "Discovery";
+
+        questions[1][0] = "Among the following identify the one in which dimensionality reduction reduces?";
+        questions[1][1] = "Performance";
+        questions[1][2] = "Entropy";
+        questions[1][3] = "Stochastic";
+        questions[1][4] = "Collinearity";
+
+        questions[2][0] = "What does K stand for in K mean algorithm?";
+        questions[2][1] = "number of cluster";
+        questions[2][2] = "number of data";
+        questions[2][3] = "number of attributes";
+        questions[2][4] = "number of iterations";
+
+        questions[3][0] = "Which of the following machine learning algorithm is based upon the idea of bagging?";
+        questions[3][1] = "decision tree";
+        questions[3][2] = "random-forest";
+        questions[3][3] = "Classification";
+        questions[3][4] = "regression";
+
+        questions[4][0] = "Which of the following is not a supervised learning?";
+        questions[4][1] = "PCA";
+        questions[4][2] = "Naive Bayesian";
+        questions[4][3] = "linear regression";
+        questions[4][4] = "Decision tree";
+
+        questions[5][0] = "Among the following options choose which one of the following focuses on the discovery of unknown properties on the data?";
+        questions[5][1] = "Big Data.";
+        questions[5][2] = "Data Mining";
+        questions[5][3] = "Machine Learning";
+        questions[5][4] = "Data Wrangling";
+
+        questions[6][0] = "Out of the given options, which of the following algorithms uses the least memory?";
+        questions[6][1] = "DFS";
+        questions[6][2] = "BFS";
+        questions[6][3] = "Both A and B are the same";
+        questions[6][4] = "can not be compared";
+
+        questions[7][0] = "Among the following logic function, which of the following cannot be implemented by a  perceptron having two inputs?";
+        questions[7][1] = "XOR";
+        questions[7][2] = "OR";
+        questions[7][3] = "NOR";
+        questions[7][4] = "AND";
+
+        questions[8][0] = "Identify the activation function output which is zero centered?";
+        questions[8][1] = "Hyperbolic Tangent";
+        questions[8][2] = "Rectified linear unit";
+        questions[8][3] = "Sigmoid";
+        questions[8][4] = "Softmax";
+
+        questions[9][0] = "Among the following SGD  variant, which of the following is based on both Momentum and adaptive learning?";
+        questions[9][1] = "RMSprop";
+        questions[9][2] = "Adam";
+        questions[9][3] = "Nesterov";
+        questions[9][4] = "Adagrad";
+        
+        answers[0][1] = "Communication Building";
+        answers[1][1] = "Collinearity";
+        answers[2][1] = "number of iterations";
+        answers[3][1] = "random-forest";
+        answers[4][1] = "PCA";
+        answers[5][1] = "Data Mining";
+        answers[6][1] = "DFS";
+        answers[7][1] = "XOR";
+        answers[8][1] = "Hyperbolic Tangent";
+        answers[9][1] = "Adam";
+        
+        opt1 = new JRadioButton();
+        opt1.setBounds(170, 520, 700, 30);
+        opt1.setBackground(Color.WHITE);
+        opt1.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt1);
+        
+        opt2 = new JRadioButton();
+        opt2.setBounds(170, 560, 700, 30);
+        opt2.setBackground(Color.WHITE);
+        opt2.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt2);
+        
+        opt3 = new JRadioButton();
+        opt3.setBounds(170, 600, 700, 30);
+        opt3.setBackground(Color.WHITE);
+        opt3.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt3);
+        
+        opt4 = new JRadioButton();
+        opt4.setBounds(170, 640, 700, 30);
+        opt4.setBackground(Color.WHITE);
+        opt4.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt4);
+        
+        groupoptions = new ButtonGroup();
+        groupoptions.add(opt1);
+        groupoptions.add(opt2);
+        groupoptions.add(opt3);
+        groupoptions.add(opt4);
+        
+        next = new JButton("Next");
+        next.setBounds(1100, 550, 200, 40);
+        next.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        next.setBackground(new Color(30, 144, 255));
+        next.setForeground(Color.WHITE);
+        next.addActionListener(this);
+        add(next);
+        
+        lifeline = new JButton("50-50 Lifeline");
+        lifeline.setBounds(1100, 630, 200, 40);
+        lifeline.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        lifeline.setBackground(new Color(30, 144, 255));
+        lifeline.setForeground(Color.WHITE);
+        lifeline.addActionListener(this);
+        add(lifeline);
+        
+        submit = new JButton("Submit");
+        submit.setBounds(1100, 710, 200, 40);
+        submit.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        submit.setEnabled(false);
+        add(submit);
+        
+        start(count);
+        
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == next) {
+            repaint();
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+               useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            
+            count++;
+            start(count);
+        } else if (ae.getSource() == lifeline) {
+            if (count == 2 || count == 4 || count == 6 || count == 8 || count == 9) {
+                opt2.setEnabled(false);
+                opt3.setEnabled(false);
+            } else {
+                opt1.setEnabled(false);
+                opt4.setEnabled(false);
+            }
+            lifeline.setEnabled(false);
+        } else if (ae.getSource() == submit) {
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+                useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+
+            for (int i = 0; i < useranswers.length; i++) {
+                if (useranswers[i][0].equals(answers[i][1])) {
+                    score += 10;
+                } else {
+                    score += 0;
+                }
+            }
+            setVisible(false);
+            new Score(name, score);
+        }
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        String time = "Time left - " + timer + " seconds"; // 15
+        g.setColor(Color.RED);
+        g.setFont(new Font("Tahoma", Font.BOLD, 25));
+        
+        if (timer > 0) { 
+            g.drawString(time, 1100, 500);
+        } else {
+            g.drawString("Times up!!", 1100, 500);
+        }
+        
+        timer--; // 14
+        
+        try {
+            Thread.sleep(1000);
+            repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        if (ans_given == 1) {
+            ans_given = 0;
+            timer = 15;
+        } else if (timer < 0) {
+            timer = 15;
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            if (count == 9) { // submit button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                
+                for (int i = 0; i < useranswers.length; i++) {
+                    if (useranswers[i][0].equals(answers[i][1])) {
+                        score += 10;
+                    } else {
+                        score += 0;
+                    }
+                }
+                setVisible(false);
+                new Score(name, score);
+            } else { // next button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                count++; // 0 // 1
+                start(count);
+            }
+        }
+        
+    }
+    
+    public void start(int count) {
+        qno.setText("" + (count + 1) + ". ");
+        question.setText(questions[count][0]);
+        opt1.setText(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]);
+        
+        opt2.setText(questions[count][2]);
+        opt2.setActionCommand(questions[count][2]);
+        
+        opt3.setText(questions[count][3]);
+        opt3.setActionCommand(questions[count][3]);
+        
+        opt4.setText(questions[count][4]);
+        opt4.setActionCommand(questions[count][4]);
+        
+        groupoptions.clearSelection();
+    }
+    
+    public static void main(String[] args) {
+        new Quiz("User");
+    }
+}
